4c5406ecdc4e605557a60f66fc4d4d200b625457
SanchezMaximo <maximosanchez420@gmail.com>
2023-06-07 14:42:02 -0300

    create a navbar responsive

--- tpi-lab-3/src/components/NavBar.js
+++ tpi-lab-3/src/components/NavBar.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./NavBar.css"
 
 
 const NavBar = () => {
+
+  const [collapsed, setCollapsed] = useState(false);
+  //arrow function para cambiar el estado del navbar
+  const handleClick = () => {
+        setCollapsed(!collapsed);
+  }
+
   return (
     <>
     <nav>
@@ -10,12 +17,17 @@ const NavBar = () => {
             <img src='./img/logo.png' alt='coffee mug ' width={60} height={60}/> 
         </a>
         <div>
-            <ul id="navbar">
-                <li><a className="active" href='index.html'>Home</a></li>
-                <li><a href='index.html'>Products</a></li>
-                <li><a href='index.html'>About</a></li>
-                <li><a href='index.html'>Contact</a></li>
+            {/* si el collapsed es true muestra el navbar mobile */}
+            <ul id="navbar" className={collapsed ? ("#navbar active"):("#navbar")}>
+                <li><a className="active" href='i'>Home</a></li>
+                <li><a href='i'>Products</a></li>
+                <li><a href='i'>About</a></li>
+                <li><a href='i'>Contact</a></li>
             </ul>
+        </div>
+        {/* cambio de logo del navbar */}
+        <div id="mobile" onClick={handleClick}>
+            <i id="bar" className={collapsed ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
     </nav>
     </>
