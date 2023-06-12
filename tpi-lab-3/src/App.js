6e316675e619a8212813ccd6032d43914190bbd1
SanchezMaximo <maximosanchez420@gmail.com>
2023-06-12 14:13:37 -0300

    navbar and home page responsive

--- tpi-lab-3/src/App.js
+++ tpi-lab-3/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 
 import {Routes, Route} from "react-router-dom";
@@ -8,24 +7,15 @@ import Products from './routes/Products';
 import About from './routes/About';
 import Contact from './routes/Contact';
 
-=======
-import "./App.css";
-import NavBar from "./components/NavBar";
->>>>>>> 323e25c5d0ebc3a840fdb8f5555762712627e1b2
-
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/products" element={<Products/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/contact" element={<Contact/>} />
       </Routes>
-=======
-      <NavBar />
->>>>>>> 323e25c5d0ebc3a840fdb8f5555762712627e1b2
     </div>
   );
 }
