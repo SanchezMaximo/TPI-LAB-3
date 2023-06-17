769f0f60b4ecde94c859acc495c4e20bf0b40821
Matias Ruiz <matiasruiz94m@gmail.com>
2023-06-17 16:04:50 -0300

    Fixed cart products

--- tpi-lab-3/src/components/ShoppingCart/ShoppingCart.js
+++ tpi-lab-3/src/components/ShoppingCart/ShoppingCart.js
@@ -1,30 +1,29 @@
-import React, { useContext, useState } from 'react'
-import { CartContext } from '../context/ShoppingCartProvider';
-import NavBar from '../Navbar/NavBar.js';
+import React, { useContext, useState } from "react";
+import { CartContext } from "../context/ShoppingCartProvider";
+import NavBar from "../Navbar/NavBar.js";
 
-import "./ShoppingCart.css"
+import "./ShoppingCart.css";
 
 const ShoppingCart = () => {
   const [cart, setCart] = useContext(CartContext);
 
   const quantity = cart.reduce((acum, current) => {
     return acum + current.quantity;
-  },0);
+  }, 0);
 
   const totalPrice = cart.reduce((acum, current) => {
     return acum + current.quantity * current.price;
-  },0);
+  }, 0);
 
   const name = cart.reduce((acum, current) => {
-    if(quantity > 1){ 
-      return acum + " " + current.name ;
+    if (quantity > 1) {
+      return acum + " " + current.name;
     } else {
-      return acum + current.name ;
+      return acum + current.name;
     }
-  },[]);
+  }, []);
 
-
-  //para mostrar card 
+  //para mostrar card
   const [show, setShow] = useState(false);
 
   const handleCheckout = () => {
@@ -35,33 +34,37 @@ const ShoppingCart = () => {
     <>
       <NavBar />
       <div className="cartContainer">
-      <div>Items in cart : {quantity}</div>
-      <button className="checkout" onClick={handleCheckout}>Checkout</button>
-      <div className={show ? "showCard" : "noShowCard"}>
-      </div>
-      {
-        quantity > 1 && show &&(
+        <div>Items in cart : {quantity}</div>
+        <button className="checkout" onClick={handleCheckout}>
+          Checkout
+        </button>
+        <div className={show ? "showCard" : "noShowCard"}></div>
+        {quantity > 1 && show && (
           <div>
             <div>Products:</div>
-            <br></br>
-            <p className='productsCart'>{name}</p>
-            <br></br>
+            <br />
+            <div className="productsCart">
+              {cart.map((product) => (
+                <p key={product.id}>{product.name}</p>
+              ))}
+            </div>
+            <br />
             <div>Total : ${totalPrice}</div>
           </div>
-        ) 
-      }
-      {
-        quantity === 1 && show &&(
+        )}
+        {quantity === 1 && show && (
           <div>
-            <div>Product: <br></br>{name}</div>
-            <br></br>
+            <div>
+              Product: <br />
+              {name}
+            </div>
+            <br />
             <div>Total : ${totalPrice}</div>
           </div>
-        ) 
-      }
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ShoppingCart  
+export default ShoppingCart;
