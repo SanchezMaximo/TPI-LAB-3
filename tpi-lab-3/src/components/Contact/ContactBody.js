34711087c4bc2bb032117e6ae2f920facdb04531
SanchezMaximo <maximosanchez420@gmail.com>
2023-06-20 16:01:22 -0300

    changes in contact

--- tpi-lab-3/src/components/Contact/ContactBody.js
+++ tpi-lab-3/src/components/Contact/ContactBody.js
@@ -7,17 +7,19 @@ const ContactBody = () => {
     <div className='contactBody'>
         <h2>Contact Us</h2>
         <div className='infoContact'>
-        <h4>Phone</h4>
-        <p>(212)9427</p>
-        <h4>Location</h4>
-        <p>Ashton New Rd, Manchester M11 3FF, Reino Unido</p>
-        <div className='hours'>
-            <h4>Hours</h4>
-            <h5>Weekdays</h5>
-            <p>7AM - 6PM</p>
-            <h5>Weekends</h5>
-            <p>8AM - 5PM</p>
+          <h4>Phone</h4>
+          <p>(212)9427</p>
+        </div> 
+        <div className='locationContact'>
+          <h4>Location</h4>
+          <p>Ashton New Rd, Manchester M11 3FF, Reino Unido.</p>
         </div>
+        <div className='hoursContact'>
+              <h4>Hours</h4>
+              <h5>Weekdays</h5>
+              <p>7AM - 6PM</p>
+              <h5>Weekends</h5>
+              <p>8AM - 5PM</p>
         </div>
     </div>
   )
