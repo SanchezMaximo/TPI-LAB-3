bee4ef25033851f12e55574489579cc06efc3e15
SanchezMaximo <maximosanchez420@gmail.com>
2023-06-23 16:45:50 -0300

    css login and sigIn

--- tpi-lab-3/src/components/Navbar/NavBar.js
+++ tpi-lab-3/src/components/Navbar/NavBar.js
@@ -51,18 +51,18 @@ const NavBar = ({ children }) => {
                 </button>
               </>
             ) : (
-              <div>
+              <div className="registrationNavBar"> 
                 <li
-                  className="buttonLogIn fa-solid fa-right-to-bracket"
+                  className="buttonLogIn"
                   onClick={() => navigate("/login")}
                 >
-                  Log in
+                  <svg className="iconLogIn" width="30" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M13.033 2v-2l10 3v18l-10 3v-2h-9v-7h1v6h8v-18h-8v7h-1v-8h9zm1 20.656l8-2.4v-16.512l-8-2.4v21.312zm-3.947-10.656l-3.293-3.293.707-.707 4.5 4.5-4.5 4.5-.707-.707 3.293-3.293h-9.053v-1h9.053z"/></svg>Log In 
                 </li>
                 <li
-                  className="buttonLogout fa-solid fa-right-to-bracket"
+                  className="buttonSignUp"
                   onClick={() => navigate("/signup")}
                 >
-                  Sign Up
+                 <svg className="iconLogIn" width="30" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M13.033 2v-2l10 3v18l-10 3v-2h-9v-7h1v6h8v-18h-8v7h-1v-8h9zm1 20.656l8-2.4v-16.512l-8-2.4v21.312zm-3.947-10.656l-3.293-3.293.707-.707 4.5 4.5-4.5 4.5-.707-.707 3.293-3.293h-9.053v-1h9.053z"/></svg> Sign Up
                 </li>
               </div>
             )}
